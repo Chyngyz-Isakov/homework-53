98ef6196d96a60bee500d9eaefe04489e776cc41
Chyngyz Isakov <ellegantus@gmail.com>
2023-05-25 14:22:33 +0600

    make addTask make removeTask

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,55 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
+import AddTaskForm from "./AddTaskForm/AddTaskForm";
+import Task from "./Task/Task";
+import {nanoid} from "nanoid";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+
+const App = () => {
+    const [tasks, setTasks] = useState([
+        {text: 'Make homework-53', id: '123'},
+        {text: 'Play a games', id: '12323'},
+        {text: 'Watch films', id: '442421'},
+    ]);
+
+    const [currentTask, setCurrentTask] = useState('')
+    const changeText = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setCurrentTask(event.target.value);
+    };
+
+    const addTask = (event: React.MouseEvent<HTMLFormElement>) => {
+        event.preventDefault();
+        const newTask = {
+            id: nanoid(),
+            text: currentTask
+        };
+        setTasks([...tasks, newTask]);
+    };
+
+    const removeTask = (id: string) => {
+        const taskCopy = [...tasks];
+        const index = tasks.findIndex(task => task.id === id);
+        taskCopy.splice(index, 1);
+        setTasks(taskCopy);
+    };
+
+
+    return (
+        <div className="App">
+            <div className='todo-wrap'>
+                <h2>To Do List:</h2>
+                <AddTaskForm onAddTask={event => addTask(event)}
+                             onChangeText={event => changeText(event)}
+                />
+                {tasks.map((task) => (
+                    <Task text={task.text}
+                          onRemoveTask={() => removeTask(task.id)}
+                          key={task.id}
+                    />
+                ))}
+            </div>
+        </div>
+    );
+};
 
 export default App;
